bb54d9c52fb6e6aff720a5b50c945105d3487d07
Samuel Cherenet <samuelcherenet1@gmail.com>
2026-05-17 21:37:43 +0300

    Add recipe bloc events

---
+++ lib/bloc/recipe_event.dart
@@ -0,0 +1,24 @@
+import '../models/recipe.dart';
+
+abstract class RecipeEvent {}
+
+class FetcchRecipe extends RecipeEvent{}
+
+class AddRecipe extends RecipeEvent{
+  final Recipe recipe;
+
+  AddRecipe(this.recipe);
+}
+
+class UpdateRecipe extends RecipeEvent {
+  final int index;
+  final Recipe recipe;
+
+  UpdateRecipe(this.index, this.recipe);
+}
+
+class DeleteRecipe extends RecipeEvent {
+  final int id;
+
+  DeleteRecipe(this.id);
+}
