6af1d36efc95369c3371c395a3d30a6036577edb
Samuel Cherenet <samuelcherenet1@gmail.com>
2026-05-17 20:07:05 +0300

    Create Recipe data model

---
+++ lib/models/recipe.dart
@@ -0,0 +1,39 @@
+class Recipe {
+ final int id;
+ final String name;
+ final String image;
+ final String description;
+ final String cuisine;
+ final String difficulty;
+
+  Recipe({
+    required this.id,
+    required this.name,
+    required this.image,
+    required this.description,
+    required this.cuisine,
+    required this.difficulty,
+  });
+
+
+  factory Recipe.fromJson(Map<String, dynamic> json) {
+    return Recipe(
+      id: json['id'],
+      name: json['name'],
+      image: json['image'],
+      description: json['description'],
+      cuisine: json['cuisine'],
+      difficulty: json['difficulty'],
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'name': name,
+      'image': image,
+      'description': description,
+      'cuisine': cuisine,
+      'difficulty': difficulty,
+    };
+  }
+}
