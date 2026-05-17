9b6b1c31e58fcc0fc8bf8bfbcdffe91faa62c8f1
Samuel Cherenet <samuelcherenet1@gmail.com>
2026-05-17 21:42:13 +0300

    Add recipe bloc states

---
+++ lib/bloc/recipe_state.dart
@@ -0,0 +1,17 @@
+import '../models/recipe.dart';
+
+abstract class RecipeState {}
+
+class RecipeLoading extends RecipeState {}
+
+class RecipeLoaded extends RecipeState {
+  final List<Recipe> recipes;
+
+  RecipeLoaded(this.recipes);
+}
+
+class RecipeError extends RecipeState {
+  final String message;
+
+  RecipeError(this.message);
+}
